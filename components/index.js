2278653672a8d3c6b44b63713ebade8b4075d685
Ngoc Tram <ntram2k3@gmail.com>
2024-08-25 17:16:13 +0700

    fix merge with klam

--- components/index.js
+++ components/index.js
@@ -6,7 +6,6 @@ import InfoBox from "./InfoBox";
 import Loader from "./Loader";
 // import Trending from "./Trending";
 import SearchInput from "./SearchInput";
-import EmptyState from "./EmptyState";
 import FooterAuth from "./FooterAuth";
 import HeaderAuth from "./HeaderAuth";
 import Notification from "./Notification";
@@ -18,7 +17,6 @@ export {
   InfoBox,
   Loader,
   SearchInput,
-  EmptyState,
   FooterAuth,
   HeaderAuth,
   AnimatedImage,
--- constants/images.js
+++ constants/images.js
@@ -2,9 +2,9 @@
 import profile from "../assets/images/profile.png";
 import logoBig from "../assets/images/logo/Big.png";
 import logoMedium from "../assets/images/logo/Medium.png";
+import logoSmall from "../assets/images/logo/Small.png";
 import robot from "../assets/images/robot.png";
 
-import empty from "../assets/images/empty.png";
 import noItem from "../assets/images/no-item.png"
 import turn1 from "../assets/images/turn/turn1.png"
 import turn2 from "../assets/images/turn/turn2.png"
@@ -12,6 +12,6 @@ import turn3 from "../assets/images/turn/turn3.png"
 import friends from "../assets/images/user-friends.png"
 import facebook from "../assets/images/facebook.png"
 
-export default { profile, thumbnail, cards, path, 
-    logoBig,logoMedium, logoSmall, empty, noItem,
+export default { profile,
+    logoBig,logoMedium, logoSmall, noItem,
     turn1, turn2, turn3, friends,facebook };
